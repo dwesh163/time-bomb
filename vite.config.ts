9a74bba4624f20577d2ed62dbf1fb30203914dc3
dwesh163 <dwesh163@protonmail.com>
2025-10-10 14:42:17 +0200

    [refactor] remove server configuration from Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     base: '/time-bomb/',
     plugins: [react()],
-    server: {
-        host: true,
-        port: 5174,
-        watch: {
-            usePolling: true,
-        },
-    },
 });
